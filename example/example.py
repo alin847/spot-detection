35bb1d82fc793a065cc645666ed6caf7a34f77db
Anthony Lin <anthonylin@MDG69V956P.tld>
2025-04-21 21:03:54 -0400

    updated example.py

--- example/example.py
+++ example/example.py
@@ -1,13 +1,13 @@
 import cv2
-from CellDetection import CellDetection
+from SpotDetection import SpotDetection
 
 
 # Step 1: create CellDetection Object
 gray_image = cv2.imread("example.tif", cv2.IMREAD_ANYDEPTH)
-image = CellDetection(gray_image=gray_image)
+image = SpotDetection(gray_image=gray_image)
 
 # Step 2a: call spot detection function
-binary_image = image.spot_detection(scales=4, threshold=6)
+binary_image = image.detection(scales=4, threshold=6)
 
 # Step 2b: check if binary image looks good
 cv2.imshow("binary image", binary_image)
@@ -17,7 +17,7 @@ cv2.waitKey()
 locations = image.localization(region_size=50, min_distance=5)
 
 # Step 3b: check if cell detection looks good
-outlined_image = image.view_locations()
+outlined_image = image.view_locations(auto=False)
 cv2.imshow("outlined image", outlined_image)
 cv2.waitKey()
 
